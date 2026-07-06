caad261c1dd19ad9e07359ebf1868cb4b993c541
Szymon Brycki <szymon.brycki@gmail.com>
2026-07-07 01:14:17 +0200

    Add files via upload

---
+++ RandomNumber/MyRandomNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomNumber;
+
+public class MyRandomNumber
+{
+    private readonly Random _myRandom = new();
+
+    public int MyRandom()
+    {
+        int randomNumber = _myRandom.Next(101);
+        return randomNumber;
+    }
+}
---
+++ RandomNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RandomNumber;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        MyRandomNumber myNumber = new MyRandomNumber();      
+        int myInt = myNumber.MyRandom();
+        Console.WriteLine($"My random number between 0 and 100 is {myInt}");
+    }
+}
